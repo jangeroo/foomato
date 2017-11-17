510dd46ddc5dcf38a8952a84e7f9d132a7b1bc92
Michael Jang <michaeljang86@gmail.com>
2017-11-16 20:44:07 -0500

    a bit of cleanup and fixed a broken function

--- firebase-backend.js
+++ firebase-backend.js
@@ -10,7 +10,6 @@ var database = admin.database()
 
 let restaurants = database.ref('/restaurants')
 
-
 function genUID() {
     return Math.floor(Math.random() * 100000000)
 }
@@ -47,92 +46,81 @@ function createMenuItem(restoID, burgerName, price) {
 
 }
 
+// Returns a menu object
 async function getMenu(restoID) {
     const menu = await restaurants.child(restoID).child('menu').once('value')
     return menu.val()
 }
 
-
-// Returns a list of burger objects
-
-
-
+// Returns a list of menu objects
 async function getAllMenus() {
     let menus = []
     for (const restoID in await getRestaurants()) {
         const menu = await getMenu(restoID)
-        // console.log(menu)
         if (menu) menus = menus.concat(menu)
-        // console.log("allBurgers ", allBurgers);
     }
     return menus;
 }
 
-async function getAllBurger() {
+// Returns a list of burger objects
+async function getAllBurgers() {
     let burgers = []
-    for (const menu in await getAllMenus()) {
-        const burger = await getAllMenus(menu)
-        // console.log(burger)
-        if (burger) burgers = burgers.concat(burger)
+    for (const menu of await getAllMenus()) {
+        for (var dish_id in menu) {
+            burgers.push(menu[dish_id])
+        }
     }
     return burgers;
 }
 
 // Returns a list of burgers objects sorted by price
 async function sortBurgersByPrice() {
-    const allB = await getAllBurger()
-    return allB.sort(function(burger1, burger2) {
+    const allBurgers = await getAllBurgers()
+    return allBurgers.sort(function(burger1, burger2) {
         return burger1.price - burger2.price
-    }
-    )
-
+    })
 }
 
 
-
 module.exports = {
+    genUID,
     createRestaurant,
+    getRestaurants,
     createMenuItem,
+    getMenu,
     getAllMenus,
-    sortBurgersByPrice
+    getAllBurgers,
+    sortBurgersByPrice,
 }
 
 
 async function runTests() {
     await database.ref("/").set(null)
 
+    // test createRestaurant() and getRestaurants()
     let resto1 = await createRestaurant('A&W', 45.5047528, -73.5725866)
     let resto2 = await createRestaurant("McDonald's", 45.5033042, -73.5694826)
     let resto3 = await createRestaurant("Burger King", 45.504193, -73.5683899)
     let restaurants = await getRestaurants()
-
     assert(Object.keys(restaurants).length === 3)
 
+    // test createMenuItem(), getMenu(), getAllMenus()
     await createMenuItem(resto1, "Momma Burger", 4.49)
     await createMenuItem(resto1, "Teen Burger", 1.99)
     await createMenuItem(resto2, "Big Mac", 3.99)
     restaurants = await getRestaurants()
-    
-    const menu = await getMenu(resto1)
-    
-    assert(Object.keys(menu).length === 2)
-    
-
-    assert(Object.keys(await getMenu(resto2)).length === 1)
-    
+    const menu1 = await getMenu(resto1)
+    const menu2 = await getMenu(resto2)
+    assert(Object.keys(menu1).length === 2)
+    assert(Object.keys(menu2).length === 1)
     const allMenus = await getAllMenus()
-
     assert(allMenus.length === 2)
-    
+
+    let allBurgers = await getAllBurgers()
+    assert(Object.keys(allBurgers).length === 3)
     // let burgersSortedByPrice = await sortBurgersByPrice(allBurgers)
     // assert(burgersSortedByPrice[0].price === 1.99)
     // assert(burgersSortedByPrice[2].price === 4.49)
-
-    // console.log("ok")
-    const burgerr = await sortBurgersByPrice()
-    console.log("burgerr ", burgerr)
-
-    
 
     console.log("All test passed")
 }
