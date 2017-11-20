1fab36df29f5eb9c954d9516de10088ecd26143a
Michael Jang <michaeljang86@gmail.com>
2017-11-20 01:08:15 -0500

    Generate data for testing
      * Generates specified number of restaurants with up to 3 burgers each.

--- createTestData.js
+++ createTestData.js
@@ -4,19 +4,6 @@ const FOOMATO_URL = 'https://foomato.herokuapp.com'
 const api_restaurant = FOOMATO_URL + '/restaurant'
 const api_dailymenu = FOOMATO_URL + '/dailymenu'
 
-
-function genLocation() {
-  const MIN_LNG = -73.75
-  const MAX_LNG = -73.53
-  const MIN_LAT = 45.415
-  const MAX_LAT = 45.555
-  return {
-    lat: MIN_LAT + (Math.random() * (MIN_LAT - MAX_LAT) + (MAX_LAT - MIN_LAT)),
-    lng: MIN_LNG + (Math.random() * (MIN_LNG - MAX_LNG) + (MAX_LNG - MIN_LNG))
-  }
-}
-
-
 function POST_dailymenu(dish) {
   return request.post(api_dailymenu).type('form').send(dish)
 }
@@ -32,10 +19,60 @@ function POST_restaurant(restaurant) {
 }
 
 
-let restaurants = []
+function genLocation() {
+  const MIN_LAT = 45.415
+  const MAX_LAT = 45.555
+  const MIN_LNG = -73.75
+  const MAX_LNG = -73.53
 
-mapRestaurants = async (restaurant) => {
-  let res_id = (await POST_restaurant(restaurant)).body.id
-  return res_id
+  lat = MIN_LAT + (Math.random() * (MIN_LAT - MAX_LAT) + (MAX_LAT - MIN_LAT))
+  lng = MIN_LNG + (Math.random() * (MIN_LNG - MAX_LNG) + (MAX_LNG - MIN_LNG))
+
+  // Limit precision to 6 decimal places to avoid coordinates
+  // that are too precise to be found
+  return {
+    lat: parseFloat(lat.toFixed(6)),
+    lng: parseFloat(lng.toFixed(6))
+  }
 }
-let res_ids = restaurants.map(mapRestaurants)
+
+function genRestaurantDetails() {
+  let location = genLocation()
+  return {
+    name: `Burger Joint ${Math.random()}`,
+    latitude: location.lat,
+    longitude: location.lng
+  }
+
+}
+
+async function generateRestaurants(numRestaurants) {
+  let restaurants = Array.from(Array(numRestaurants).keys()).map(genRestaurantDetails)
+  mapRestaurants = async (restaurant) => {
+    let res_id = (await POST_restaurant(restaurant)).body.id
+    return res_id
+  }
+  let promises = restaurants.map(mapRestaurants)
+  return Promise.all(promises)
+}
+
+async function generateBurgers(res_ids) {
+  async function generateMenu(res_id) {
+    let numBurgers = Math.floor(Math.random() * 3) + 1
+    for (let i = 0; i < numBurgers; i++) {
+      await POST_dailymenu({
+        res_id,
+        name: `Burger ${Math.random()}`,
+        price: Math.floor(Math.random() * 1000) / 100
+      })
+    }
+  }
+  res_ids.forEach(generateMenu)
+}
+
+async function createTestData(numRestaurants) {
+  let res_ids = await generateRestaurants(numRestaurants)
+  await generateBurgers(res_ids)
+}
+
+createTestData(5)
